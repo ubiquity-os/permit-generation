de2330129fe709292e4598f81cb1461ac43c0df6
whilefoo <whilefoo@users.noreply.github.com>
2024-04-08 19:28:12 +0200

    feat: return data to kernel

--- src/index.ts
+++ src/index.ts
@@ -60,11 +60,24 @@ async function run() {
     await handleSlashCommands(context, octokit);
   } else {
     const permits = await generatePayoutPermit(context, settings.permitRequests);
-    // TODO: return permits to kernel
+    await returnDataToKernel(env.GITHUB_TOKEN, inputs.stateId, permits);
     return JSON.stringify(permits);
   }
 
   return "No permit generated";
+}
+
+async function returnDataToKernel(repoToken: string, stateId: string, output: object) {
+  const octokit = new Octokit({ auth: repoToken });
+  await octokit.repos.createDispatchEvent({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    event_type: "return_data_to_ubiquibot_kernel",
+    client_payload: {
+      state_id: stateId,
+      output: JSON.stringify(output),
+    },
+  });
 }
 
 async function handleSlashCommands(context: Context, octokit: Octokit) {
