902969662e3f9513546641ea5972e549c1a70fbe
gentlementlegen <fernand.veyrier@epitech.eu>
2024-04-08 16:38:07 +0900

    chore: encode decode functions for permit payloads

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["build/index.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts"],
+  ignore: ["src/types/config.ts", "src/adapters/supabase/types/database.ts"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: [],
 };
