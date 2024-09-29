133090e55639d6d6ef13f5b9c40561713ff9b23a
hh <hhio618@gmail.com>
2024-09-29 21:14:05 +0330

    chore: add more logs
    chore: test

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const crypto = require("crypto");
+import { createClient } from "@supabase/supabase-js";
 
-// Generate an X25519 key pair
-const { privateKey } = crypto.generateKeyPairSync("x25519");
-
-// Export the private key as a raw Buffer
-const privateKeyBuffer = privateKey.export({ type: "pkcs8", format: "der" });
-
-// Convert the Buffer to a hexadecimal string
-const privateKeyHex = privateKeyBuffer.toString("hex");
-
-console.log("X25519 Private Key (Hex):", privateKeyHex);
+const supabaseUrl = "https://uorkbrlsegsfjspkluwj.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
