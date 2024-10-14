99d2febcf15d8eb3eda582f49b77fd830380a4f7
hh <hhio618@gmail.com>
2024-10-14 20:26:39 +0330

    feat: use uuid for nonce

--- src/handlers/generate-erc20-permit.ts
+++ src/handlers/generate-erc20-permit.ts
@@ -4,6 +4,7 @@ import { Context, Logger } from "../types/context";
 import { PermitReward, TokenType } from "../types";
 import { decrypt, parseDecryptedPrivateKey } from "../utils";
 import { getFastestProvider } from "../utils/get-fastest-provider";
+import { v4 as uuidv4 } from "uuid";
 
 export interface Payload {
   evmNetworkId: number;
@@ -55,7 +56,7 @@ export async function generateErc20PermitSignature(
     } else if ("pull_request" in contextOrPayload.payload) {
       issueNodeId = contextOrPayload.payload.pull_request.node_id;
     } else if (contextOrPayload.config.runId) {
-      issueNodeId = contextOrPayload.config.runId.toString();
+      issueNodeId = uuidv4();
     } else {
       throw new Error("Issue Id is missing");
     }
