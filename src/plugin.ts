5df1caa2c15b984be55f1f37b64897b77d383046
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-30 21:53:41 +0000

    chore: update runPlugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,11 @@
 import { Octokit } from "@octokit/rest";
 import { Env, PluginInputs } from "./types";
 import { Context } from "./types";
-import { generatePayoutPermit } from "./handlers";
 import { returnDataToKernel } from "./helpers/validator";
-import { Logs } from "@ubiquity-os/ubiquity-os-logger";
 import { createAdapters } from "./adapters";
 import { createClient } from "@supabase/supabase-js";
-
-export async function runPlugin(context: Context) {
-  return await generatePayoutPermit(context, context.config.permitRequests);
-}
+import { generatePayoutPermits } from "./handlers";
+import { logger } from "./helpers/logger";
 
 export async function plugin(inputs: PluginInputs, env: Env) {
   const octokit = new Octokit({ auth: inputs.authToken });
@@ -21,11 +17,11 @@ export async function plugin(inputs: PluginInputs, env: Env) {
     config: inputs.settings,
     octokit,
     env,
-    logger: new Logs("debug"),
+    logger: logger,
     adapters: {} as ReturnType<typeof createAdapters>,
   };
 
-  context.adapters = createAdapters(supabase, context);
+  context.adapters = createAdapters(supabase);
 
-  return returnDataToKernel(context, inputs.authToken, inputs.stateId, await runPlugin(context));
+  return returnDataToKernel(context, inputs.authToken, inputs.stateId, await generatePayoutPermits(context));
 }
