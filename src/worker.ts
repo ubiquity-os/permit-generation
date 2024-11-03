219deb4ca6c115cef1e15e73b33a8257d103e8cb
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-03 22:01:36 +0000

    chore: return permits json

--- src/worker.ts
+++ src/worker.ts
@@ -3,6 +3,21 @@ import { validateAndDecodeSchemas } from "./helpers/validator";
 import { plugin } from "./plugin";
 import { Env } from "./types";
 
+/**
+ * Cloudflare workers run on edge servers so this is not supported in the worker runtime,
+ * it is only available in the browser runtime and will throw an error if removed.
+ *
+ * Libsodium is problematic in workers. It is a fork of the nacl lib and abstracts
+ * away the complexities of using it. To solve this, tweetnacl is used instead of libsodium
+ * to do the heavy lifting of the nacl lib, while sodium is used for it's utilities.
+ *
+ * TweetNacl was written by the core authors of the original NaCl lib in C, and is a small lib
+ * that fits inside 100 tweets. It is closer to the metal than libsodium, and is more
+ * suitable for use in a worker environment.
+ *
+ * @ts-expect-error globalThis*/
+globalThis.importScripts = undefined;
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
@@ -37,8 +52,8 @@ export default {
       const { decodedSettings, decodedEnv } = validateAndDecodeSchemas(env, webhookPayload.settings);
       webhookPayload.env = decodedEnv;
       webhookPayload.settings = decodedSettings;
-      await plugin(webhookPayload, decodedEnv);
-      return new Response(JSON.stringify("OK"), { status: 200, headers: { "content-type": "application/json" } });
+      const permits = await plugin(webhookPayload, decodedEnv);
+      return new Response(JSON.stringify(permits), { status: 200, headers: { "content-type": "application/json" } });
     } catch (error) {
       return handleUncaughtError(error);
     }
