e89fc6343a75d1f32f0d777cfef61b4993f711a3
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-03 22:00:55 +0000

    chore: return params from decrypt

--- src/helpers/signer.ts
+++ src/helpers/signer.ts
@@ -4,13 +4,13 @@ import { logger } from "./logger";
 
 export async function getPrivateKey(evmPrivateEncrypted: string, x25519privateKey: string) {
   try {
-    const privateKeyDecrypted = await decrypt(evmPrivateEncrypted, x25519privateKey);
+    const { privateKey: privateKeyDecrypted } = await decrypt(evmPrivateEncrypted, x25519privateKey);
     const privateKeyParsed = parseDecryptedPrivateKey(privateKeyDecrypted);
     const privateKey = privateKeyParsed.privateKey;
     if (!privateKey) throw new Error("Private key is not defined");
     return privateKey;
   } catch (error) {
-    const errorMessage = `Failed to decrypt a private key: ${error}`;
+    const errorMessage = `[getPrivateKey] - Failed to decrypt a private key: ${error}`;
     logger.error(errorMessage);
     throw new Error(errorMessage);
   }
