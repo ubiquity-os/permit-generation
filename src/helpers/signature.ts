f0a3ebe09ad2496567fdf7bcf0073785f75bc535
Kingsley Victor <javaprodigy56@gmail.com>
2024-11-03 23:42:25 +0100

    chore: start #https://github.com/ubiquity-os/permit-generation/issues/71

---
+++ src/helpers/signature.ts
@@ -0,0 +1,43 @@
+interface Inputs {
+  stateId: string;
+  eventName: string;
+  eventPayload: unknown;
+  authToken: string;
+  settings: unknown;
+  ref: unknown;
+}
+
+export async function verifySignature(publicKeyPem: string, inputs: Inputs, signature: string) {
+  try {
+    const inputsOrdered = {
+      stateId: inputs.stateId,
+      eventName: inputs.eventName,
+      eventPayload: inputs.eventPayload,
+      settings: inputs.settings,
+      authToken: inputs.authToken,
+      ref: inputs.ref,
+    };
+
+    const pemContents = publicKeyPem.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").trim();
+    const binaryDer = Uint8Array.from(atob(pemContents), (c) => c.charCodeAt(0));
+
+    const publicKey = await crypto.subtle.importKey(
+      "spki",
+      binaryDer,
+      {
+        name: "RSASSA-PKCS1-v1_5",
+        hash: "SHA-256",
+      },
+      true,
+      ["verify"]
+    );
+
+    const signatureArray = Uint8Array.from(atob(signature), (c) => c.charCodeAt(0));
+    const dataArray = new TextEncoder().encode(JSON.stringify(inputsOrdered));
+
+    return await crypto.subtle.verify("RSASSA-PKCS1-v1_5", publicKey, signatureArray, dataArray);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
