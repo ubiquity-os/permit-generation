2aff45b88acb4e62efed03a718ca8704b6f6ff76
Kingsley Victor <javaprodigy56@gmail.com>
2024-11-10 12:24:54 +0100

    chore: fix knip complains

--- knip.ts
+++ knip.ts
@@ -3,10 +3,10 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["src/main.ts"],
   project: ["src/**/*.ts"],
-  ignore: [],
+  ignore: ["src/plugin.ts", "src/worker.ts", "src/helpers/signature.ts", "src/types/webhook-payload.ts"],
   ignoreBinaries: ["publish"],
   ignoreExportsUsedInFile: true,
-  ignoreDependencies: ["ts-node"],
+  ignoreDependencies: ["ts-node", "supabase"],
   jest: {
     config: ["jest.config.ts"],
     entry: ["tests/*.ts"],
