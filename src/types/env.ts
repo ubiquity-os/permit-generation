686c05e241925fc3b7ed688ff4e43c7b2afecb4a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-27 19:10:36 +0000

    chore: format

--- src/types/env.ts
+++ src/types/env.ts
@@ -6,18 +6,22 @@ export const envSchema = T.Object({
   GITHUB_TOKEN: T.String(),
   SUPABASE_URL: T.String(),
   SUPABASE_KEY: T.String(),
-  NFT_MINTER_PRIVATE_KEY: T.Transform(T.Union([T.Undefined(), T.String()])).Decode((val) => {
-    if (!val) {
-      return "" // Being used correctly as a falsy value
-    }
-    return val
-  }).Encode((val) => val),
-  NFT_CONTRACT_ADDRESS: T.Transform(T.Union([T.Undefined(), T.String()])).Decode((val) => {
-    if (!val) {
-      return "" // Being used correctly as a falsy value
-    }
-    return val
-  }).Encode((val) => val),
+  NFT_MINTER_PRIVATE_KEY: T.Transform(T.Union([T.Undefined(), T.String()]))
+    .Decode((val) => {
+      if (!val) {
+        return ""; // Being used correctly as a falsy value
+      }
+      return val;
+    })
+    .Encode((val) => val),
+  NFT_CONTRACT_ADDRESS: T.Transform(T.Union([T.Undefined(), T.String()]))
+    .Decode((val) => {
+      if (!val) {
+        return ""; // Being used correctly as a falsy value
+      }
+      return val;
+    })
+    .Encode((val) => val),
   X25519_PRIVATE_KEY: T.String(),
 });
 
