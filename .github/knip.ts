62b3c5249963a2416b0dc7ec425de375ab887ad2
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-11 07:40:00 +0000

    chore: update workflows, remove tsx

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,12 +1,12 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["build/index.ts"],
+  entry: ["build/index.ts", "src/worker.ts"],
   project: ["src/**/*.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "src/plugin.ts", "src/worker.ts", "src/helpers/validator.ts"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["@mswjs/data", "@typescript-eslint/eslint-plugin", "@typescript-eslint/parser", "ts-node", "tsx", "node"],
+  ignoreDependencies: ["@mswjs/data", "@typescript-eslint/eslint-plugin", "@typescript-eslint/parser", "ts-node", "node"],
   eslint: true,
   ignoreBinaries: ["publish"],
 };
