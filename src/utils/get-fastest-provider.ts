db808bb6a14456b5473d285bb4ec1588e925b021
gentlementlegen <fernand.veyrier@epitech.eu>
2025-06-12 17:22:40 +0300

    chore: added provider block test

--- src/utils/get-fastest-provider.ts
+++ src/utils/get-fastest-provider.ts
@@ -2,7 +2,10 @@ import { ethers, providers } from "ethers";
 
 export async function getRpcProvider(networkId: number | string): Promise<providers.JsonRpcProvider> {
   try {
-    return new ethers.providers.JsonRpcProvider(`https://rpc.ubq.fi/${networkId}`);
+    const provider = new ethers.providers.JsonRpcProvider(`https://rpc.ubq.fi/${networkId}`);
+    // We make one call to make sure our provider is responding
+    await provider.getBlockNumber();
+    return provider;
   } catch (e) {
     throw new Error(`Failed to get provider for networkId: ${networkId}`);
   }
